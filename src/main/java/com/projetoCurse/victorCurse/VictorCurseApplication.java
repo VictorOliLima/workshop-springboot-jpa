715b310283272f0ad5e71a4be8ca2db2ddcc390e
Victor Lima <victorhugolima2909@gmail.com>
2025-07-17 16:08:57 -0300

    created project

---
+++ src/main/java/com/projetoCurse/victorCurse/VictorCurseApplication.java
@@ -0,0 +1,13 @@
+package com.projetoCurse.victorCurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorCurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorCurseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoCurse/victorCurse/VictorCurseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoCurse.victorCurse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorCurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
