5b671b789995ecec7559d8bdef314dd9a3927f7a
Victor Lima <victorhugolima2909@gmail.com>
2025-07-21 16:41:34 -0300

    Add implements Category: entity, service, controler, repository and test

--- src/main/java/com/projetoCurse/victorCurse/config/TesteConfig.java
+++ src/main/java/com/projetoCurse/victorCurse/config/TesteConfig.java
@@ -1,7 +1,9 @@
 package com.projetoCurse.victorCurse.config;
 
+import com.projetoCurse.victorCurse.entities.Category;
 import com.projetoCurse.victorCurse.entities.Order;
 import com.projetoCurse.victorCurse.entities.enums.OrderStatus;
+import com.projetoCurse.victorCurse.repositories.CategoryRepository;
 import com.projetoCurse.victorCurse.repositories.OrderRepository;
 import com.projetoCurse.victorCurse.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,17 @@ public class TesteConfig implements CommandLineRunner {
         @Autowired
         private OrderRepository orderRepository;
 
+        @Autowired
+        private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronicos");
+        Category cat2 = new Category(null, "Livros");
+        Category cat3 = new Category(null, "Computadores");
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -33,6 +43,7 @@ public class TesteConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,  u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,  u1);
 
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3)); 
     }
---
+++ src/main/java/com/projetoCurse/victorCurse/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.projetoCurse.victorCurse.repositories;
+
+import com.projetoCurse.victorCurse.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
